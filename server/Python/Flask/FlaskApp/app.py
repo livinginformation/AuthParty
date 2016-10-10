c96da020b0bd6886ad92fb0b311b4653f2e72f23
livinginformation <douglas.roffel@gmail.com>
2016-10-09 21:19:08 -0700

    add user creation to db (rudimentary)

--- server/Python/Flask/FlaskApp/app.py
+++ server/Python/Flask/FlaskApp/app.py
@@ -1,20 +1,40 @@
-from flask import Flask, render_template, request, json
-from flask.ext.mysql import MySQL
-from werkzeug import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, json, g
+import sqlite3
 
+# from werkzeug import generate_password_hash, check_password_hash
+DATABASE = 'authparty.db'
 app = Flask(__name__)
-mysql = MySQL()
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'AuthParty'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('sp_createUsers.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+        with app.open_resource('sp_createSessions.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+init_db()
 
 @app.route("/")
 def main():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/showSignUp')
 def showSignUp():
@@ -22,28 +42,24 @@ def showSignUp():
 
 @app.route('/signUp', methods=['POST'])
 def signUp():
-	# read the posted values from the UI
-	_name = request.form['inputName']
-	_email = request.form['inputEmail']
-	_password = request.form['inputPassword']
+    create_user_sql = "INSERT INTO Users (USERNAME, PASSWORD, EMAIL) VALUES (?, ?, ?)"
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
 
-	# validate received values
-	if _name and _email and _password:
-		conn = mysql.connect()
-		cursor = conn.cursor()
+    # validate received values
+    if _name and _email and _password:
+        _db = get_db()
+        cur = _db.cursor()
+        cur.execute(create_user_sql, (_name, _password, _email))
+        _db.commit()
 
-		_hashed_password = generate_password_hash(_password)
-		cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
-		data = cursor.fetchall()
-		
-		if len(data) is 0:
-			conn.commit()		
-			return json.dumps({'message':'User created successfully!'})
-		else:
-			return json.dumps({'error': str(data[0])})
-	else:
-		return json.dumps({'html':'<span>Enter the required fields</span>'})
+        return json.dumps({'html':'<span>All fields valid!</span>'})
+
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
